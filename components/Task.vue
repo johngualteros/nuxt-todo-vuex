df218c1e24312135bd0e8d2041b50b07e71e1116
John Gualteros <gualterosjohn40@gmail.com>
2022-09-03 00:49:42 -0500

    final commit

---
+++ components/Task.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="`task ${task.done ? 'is-complete' : ''}`">
+    <div class="content">
+      {{ task.content }}
+    </div>
+    <div class="buttons">
+      <button @click="toggleDone">{{ task.done ? "Undo" : "Done" }}</button>
+      <button @click="deleteTask" class="delete">Delete</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["task"],
+  methods: {
+    toggleDone() {
+      this.$store.commit("TOGGLE_TASK", this.task);
+    },
+    deleteTask(task) {
+      this.$store.commit("REMOVE_TASK", this.task);
+    },
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <h1>Task Board</h1>
+    <p>Create a list of tasks</p>
+
+    <div class="create-new">
+      <input
+        type="text"
+        v-model="newTask"
+        placeholder="Add a new task"
+        @keypress.enter="addTask"
+      />
+      <button @click="addTask">ADD</button>
+    </div>
+    <div class="tasks">
+      <!-- Componente -->
+      <Task
+        v-for="(task, index) in $store.state.tasks"
+        :key="index"
+        :task="task"
+      />
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask(){
+      if (this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask)
+        this.newTask = ''
+      }   
+    }
+  },
+};
+</script>
